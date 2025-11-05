69525581dfe8eb33d2f7f6b6ec60f505038f6d46
Arthur-Amaral-cloud <arthur.asilva@sptech.school>
2025-11-05 13:17:02 -0300

    Funcionalidade(Tela Usuario) - Enviando o email para o usuário quando criado novo convite

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
 require("dotenv").config({ path: caminho_env });
 
 var express = require("express");
+const nodemailer = require('nodemailer');
 var cors = require("cors");
 var path = require("path");
 var PORTA_APP = process.env.APP_PORT;
 var HOST_APP = process.env.APP_HOST;
+var PORT = 3000;
 
 var app = express();
 
@@ -37,7 +39,39 @@ app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
 app.use("/dashboard", dashboardRouter);
-app.use("/convites", convitesRouter)
+app.use("/convites", convitesRouter);
+
+app.post('/enviar-email', async (req, res) => {
+    const {email, assunto,mensagem } = req.body;
+    const transporter = nodemailer.createTransport({
+        service: 'gmail', 
+        auth: {
+            user: process.env.EMAIL, // Coloque o email aqui
+            pass: process.env.SENHA // Coloque a senha do aplicativo aqui
+        }
+    });
+
+    const mailOptions = {
+        from: `"HealthGuard" <contatohealthguard@gmail.com>`,    
+        to: `${email}`, 
+        subject: `${assunto}`, 
+        
+        html: `
+            ${mensagem}
+        `
+    };
+    try {
+        await transporter.sendMail(mailOptions);
+        
+        res.status(200).send('E-mail enviado com sucesso!');
+
+    } catch (error) {
+        console.error('Erro ao enviar e-mail:', error);
+        res.status(500).send('Falha ao enviar o e-mail. Tente novamente.');
+    }
+});
+
+
 
 app.listen(PORTA_APP, function () {
     console.log(`
@@ -54,4 +88,10 @@ app.listen(PORTA_APP, function () {
     \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
     \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(process.env.EMAIL, process.env.SENHA)
 });
