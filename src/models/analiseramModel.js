da6223ed391347b36e40928ee12aa47a0cdee634
Arthur-Amaral-cloud <arthur.asilva@sptech.school>
2025-11-24 22:27:46 -0300

    Funcionalidade(Análise RAM) - Conectando a kpi e gráfico no banco

--- src/models/analiseramModel.js
+++ src/models/analiseramModel.js
@@ -1,6 +1,41 @@
 var database = require("../database/config");
 
+function buscarnomemaquina(idDac) {
+    var instrucaoSql = `SELECT nomeIdentificacao FROM Dac WHERE idDac = ${idDac}`;
+
+    return database.executar(instrucaoSql);
+}
+function buscarmemoria(idDac) {
+    var instrucaoSql = `SELECT fkMedicoesDisponiveis,medidaCapturada FROM Leitura WHERE fkDac = ${idDac} AND (fkMedicoesDisponiveis = 7 OR fkMedicoesDisponiveis = 9) ORDER BY idLeitura DESC LIMIT 2;`;
+
+    return database.executar(instrucaoSql);
+}
+function buscarmetricasrammaquina(idDac) {
+    var instrucaoSql = `SELECT valorMinimo,valorMaximo,nomeNivel FROM MetricaAlerta WHERE fkDac = ${idDac} AND fkMedicoesDisponiveis = 6;`;
+
+    return database.executar(instrucaoSql);
+}
+function buscarmetricasrammaquinaunidade(idUnidade) {
+    var instrucaoSql = `SELECT valorMinimo,valorMaximo,nomeNivel FROM MetricaAlerta WHERE fkUnidadeDeAtendimento = ${idUnidade} AND fkMedicoesDisponiveis = 6 AND fkDac IS NULL;`;
+
+    return database.executar(instrucaoSql);
+}
+function puxardadosgraficoRAM(idDac) {
+    var instrucaoSql = `SELECT fkMedicoesDisponiveis,medidaCapturada,dataCaptura FROM Leitura WHERE fkDac = ${idDac} AND (fkMedicoesDisponiveis = 6 OR fkMedicoesDisponiveis = 8) ORDER BY idLeitura DESC LIMIT 20`;
+
+    return database.executar(instrucaoSql);
+}
+function atualizardadosgraficoRAM(idDac) {
+    var instrucaoSql = `SELECT fkMedicoesDisponiveis,medidaCapturada,dataCaptura FROM Leitura WHERE fkDac = ${idDac} AND (fkMedicoesDisponiveis = 6 OR fkMedicoesDisponiveis = 8) ORDER BY idLeitura DESC LIMIT 2`;
+
+    return database.executar(instrucaoSql);
+}
 
 module.exports = {
-    
+    buscarnomemaquina,
+    buscarmemoria,
+    buscarmetricasrammaquina,
+    buscarmetricasrammaquinaunidade,
+    puxardadosgraficoRAM,
+    atualizardadosgraficoRAM
 };
--- src/routes/analiseram.js
+++ src/routes/analiseram.js
@@ -3,5 +3,28 @@ var router = express.Router();
 
 var analiseramController = require("../controllers/analiseramController");
 
+router.get(`/buscarnomemaquina/:idDac`, function (req, res) {
+    analiseramController.buscarnomemaquina(req, res);
+});
+
+router.get(`/buscarmemoria/:idDac`, function (req, res) {
+    analiseramController.buscarmemoria(req, res);
+});
+
+router.get(`/buscarmetricasrammaquina/:idDac`, function (req, res) {
+    analiseramController.buscarmetricasrammaquina(req, res);
+});
+
+router.get(`/buscarmetricasrammaquinaunidade/:idUnidade`, function (req, res) {
+    analiseramController.buscarmetricasrammaquinaunidade(req, res);
+});
+
+router.get(`/puxardadosgraficoRAM/:idDac`, function (req, res) {
+    analiseramController.puxardadosgraficoRAM(req, res);
+});
+
+router.get(`/atualizardadosgraficoRAM/:idDac`, function (req, res) {
+    analiseramController.atualizardadosgraficoRAM(req, res);
+});
 
 module.exports = router;
