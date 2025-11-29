6de2905b38b127a4d18c650e1590b7251f0483e1
andreleao-dev <andre.leao@sptech.school>
2025-11-29 14:46:00 -0300

    Config(Histórico de alertas geral) - Ajustando tabela e consulta que busca alertas

--- src/models/alertasgeralModel.js
+++ src/models/alertasgeralModel.js
@@ -1,24 +1,26 @@
 var database = require("../database/config");
 
 function puxarAlertasGeral(idUnidadeAtendimento){
-     var instrucaoSql = `SELECT
-                            alerta.fkUnidadeDeAtendimento, 
-                            metriA.nomeNivel AS tipoAlerta,
-                            dac.nomeIdentificacao AS nomeMaquina,
-                            dataInicio,
-                            dataTermino,
-                            statusAlerta    
-                        FROM
-                            alerta
-                            JOIN medicoesSelecionadas AS medS 
-                              ON medS.idMedicoesSelecionadas = alerta.fkMedicoesSelecionadas
-                            JOIN MedicoesDisponiveis AS medD 
-                              ON medD.idMedicoesDisponiveis = medS.fkMedicoesDisponiveis
-                            JOIN MetricaAlerta AS metriA 
-                              ON metriA.fkMedicoesDisponiveis = medD.idMedicoesDisponiveis
-                            JOIN Dac 
-                              ON metriA.fkDac = dac.idDac
-                            WHERE alerta.fkUnidadeDeAtendimento = 1`;
+     var instrucaoSql = `SELECT 
+                              idAlerta,
+                              nomeAlerta,
+                              medD.nomeDaMedicao AS monitoramento,
+                              dac.nomeIdentificacao AS nomeMaquina,
+                              dataInicio,
+                              dataTermino,
+                              statusAlerta,
+                              dataVisualizacao,
+                              nomeVisualizador AS verificador
+                          FROM
+                              alerta
+                                  JOIN
+                              medicoesSelecionadas AS medS ON medS.idMedicoesSelecionadas = alerta.fkMedicoesSelecionadas
+                                  JOIN
+                              Dac ON medS.fkDac = dac.idDac
+                                  JOIN
+                              MedicoesDisponiveis AS medD ON medD.idMedicoesDisponiveis = medS.fkMedicoesDisponiveis
+                          WHERE
+                              alerta.fkUnidadeDeAtendimento = 1;`;
     
     return database.executar(instrucaoSql);
  }
